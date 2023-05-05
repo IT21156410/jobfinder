fe1b5ac25e256e3a1ebce1e4b59a5847a697789e
hansajith18 <hansajith18@gmail.com>
2023-05-05 12:24:38 +0530

    CV_INFO class name corrected

--- app/src/main/java/com/example/jobseeker/CvInfo.kt
+++ app/src/main/java/com/example/jobseeker/CvInfo.kt
@@ -3,7 +3,7 @@ package com.example.jobseeker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CV_INFO : AppCompatActivity() {
+class CvInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cv_info)
