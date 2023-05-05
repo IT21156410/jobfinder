6a982d49d71aca821b766c2d6c9c8c2b6173693d
hansajith18 <hansajith18@gmail.com>
2023-05-05 12:44:28 +0530

    Navigation between screens

--- app/src/main/java/com/example/jobseeker/CvInfo.kt
+++ app/src/main/java/com/example/jobseeker/CvInfo.kt
@@ -2,10 +2,17 @@ package com.example.jobseeker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CvInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cv_info)
+
+        val goBackButton = findViewById<Button>(R.id.goBack)
+
+        goBackButton.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/jobseeker/MainActivity.kt
+++ app/src/main/java/com/example/jobseeker/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.jobseeker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goToCvActivityButton = findViewById<Button>(R.id.gotoCvActivity)
+        goToCvActivityButton.setOnClickListener {
+            val intent = Intent(this, CvInfo::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
 }
