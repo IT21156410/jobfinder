3bf02a9b715cde3d009d241d95615768ee1d891b
Navod Hansajith <it21156410@my.sliit.lk>
2023-05-12 12:08:25 +0530

    add education

--- app/src/main/java/com/example/jobseeker/MainActivity.kt
+++ app/src/main/java/com/example/jobseeker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jobseeker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,19 @@ class MainActivity : AppCompatActivity() {
         val goToJobSearchActivityButton = findViewById<Button>(R.id.gotoJobSearch)
         goToJobSearchActivityButton.setOnClickListener {
             val intent = Intent(this, JobSearchActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+
+        val addEducationBtnButton = findViewById<Button>(R.id.addEducationBtn)
+        addEducationBtnButton.setOnClickListener {
+            val intent = Intent(this, AddEducation::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+        val goToProfileButton = findViewById<Button>(R.id.goToProfile)
+        goToProfileButton.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
             // start your next activity
             startActivity(intent)
         }
