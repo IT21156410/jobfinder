5611c1312cfb10e2b821b6636e261995d562f511
Nishadi Nawanjala <it21156038@my.sliit.lk>
2023-05-12 19:14:26 +0530

    Ui Company/Reviews/Salary/ForgorPassword/ResetPassword

---
+++ app/src/main/java/com/example/jobseeker/CompanyInfo.kt
@@ -0,0 +1,11 @@
+package com.example.jobseeker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompanyInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_info)
+    }
+}
---
+++ app/src/main/java/com/example/jobseeker/CompanyReviews.kt
@@ -0,0 +1,11 @@
+package com.example.jobseeker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompanyReviews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_reviews)
+    }
+}
