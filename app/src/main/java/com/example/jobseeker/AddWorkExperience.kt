e9005f1aa955fbe9c97858f86cb873e6cad93c02
hansajith18 <hansajith18@gmail.com>
2023-05-12 21:46:03 +0530

    Work Experience / SignUp/ User profile

---
+++ app/src/main/java/com/example/jobseeker/AddWorkExperience.kt
@@ -0,0 +1,11 @@
+package com.example.jobseeker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddWorkExperience : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_work_experience)
+    }
+}
