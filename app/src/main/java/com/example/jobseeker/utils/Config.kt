20e2b087c73f0c85ded58d8b805b5aee6d378260
hansajith18 <hansajith18@gmail.com>
2023-05-13 07:26:19 +0530

    Loader & profile Ui

--- app/src/main/java/com/example/jobseeker/utils/Config.kt
+++ app/src/main/java/com/example/jobseeker/utils/Config.kt
@@ -10,14 +10,14 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 object Config {
     private var dialog: AlertDialog? = null
 
-//    fun showDialog(context : Context){
-//        dialog = MaterialAlertDialogBuilder(context)
-//            .setView(R.layout.loading_layout)
-//            .setCancelable(false)
-//            .create()
-//
-//        dialog!!.show()
-//    }
+    fun showDialog(context : Context){
+        dialog = MaterialAlertDialogBuilder(context)
+            .setView(R.layout.activity_loading)
+            .setCancelable(false)
+            .create()
+
+        dialog!!.show()
+    }
 
     fun hideDialog() {
 
